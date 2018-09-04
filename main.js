c5875e9a42f41c7773bce424f8b5d30c338afeb3
Suhail <suhailhussains94@gmail.com>
2018-09-04 10:53:59 +0530

    almost done, needs testing

--- main.js
+++ main.js
@@ -1,1 +1,205 @@
-console.log("This is type script file");
+var Youtube_Video = /** @class */ (function () {
+    function Youtube_Video(videoID, videoTitle, videoURL, channelName, numberOfSubscibers, description, uploadedOn, isVerified, numberOfViews, numberOfUpvotes, numberOfDownVotes, comments) {
+        if (numberOfViews === void 0) { numberOfViews = 0; }
+        if (numberOfUpvotes === void 0) { numberOfUpvotes = 0; }
+        if (numberOfDownVotes === void 0) { numberOfDownVotes = 0; }
+        if (comments === void 0) { comments = [""]; }
+        var _this = this;
+        this.getNumberOfViews = function () {
+            console.log(_this.numberOfViews);
+        };
+        this.getRecommendedVideo = function () {
+            console.log("These are your related videos");
+        };
+        this.getChannelName = function () {
+            console.log(_this.channelName);
+        };
+        this.getUpVotes = function () {
+            console.log(_this.numberOfUpvotes);
+        };
+        this.getDownVotes = function () {
+            console.log(_this.numberOfDownVotes);
+        };
+        this.getNumberOfSubscribers = function () {
+            console.log(_this.numberOfSubscribers);
+        };
+        this.getVideoTitle = function () {
+            console.log(_this.videoTitle);
+        };
+        this.getDescription = function () {
+            console.log(_this.description);
+        };
+        this.displayComments = function () {
+            var numberOfComments = _this.comments.length;
+            console.log(numberOfComments);
+            for (var x in _this.comments) {
+                console.log(_this.comments[x]);
+            }
+        };
+        this.addComments = function (comment) {
+            _this.comments.push(comment);
+        };
+        this.likeVideo = function () {
+            _this.numberOfUpvotes += 1;
+        };
+        this.hateVideo = function () {
+            _this.numberOfDownVotes += 1;
+        };
+        this.subscribeChannel = function () {
+            _this.numberOfSubscribers += 1;
+        };
+        this.shareVideo = function () {
+            console.log(_this.videoURL);
+        };
+        this.videoID = videoID;
+        this.videoTitle = videoTitle;
+        this.videoURL = videoURL;
+        this.channelName = channelName;
+        this.numberOfViews = numberOfViews;
+        this.numberOfUpvotes = numberOfUpvotes;
+        this.numberOfDownVotes = numberOfDownVotes;
+        this.numberOfSubscribers = numberOfSubscibers;
+        this.description = description;
+        this.comments = comments;
+        this.isVerified = isVerified;
+    }
+    return Youtube_Video;
+}());
+var Social_Profile = /** @class */ (function () {
+    function Social_Profile(userName, dateOfBirth, gender, interestedIn, relationship, prifilePic, phNumber, occupation, livesIn, languages, religiousViews, politicalViews, familyMembers, aboutYou, otherNames, quote, bloodgroup, isDonated, friendList) {
+        var _this = this;
+        this.getOverView = function () {
+            var lastOc = _this.occupation.length;
+            console.log(_this.occupation[lastOc - 1]);
+            var lastEd = _this.education.length;
+            console.log(_this.education[lastEd - 1]);
+            var lastCy = _this.livesIn.length;
+            console.log(_this.livesIn[lastCy - 1]);
+            for (var x in _this.phNumber) {
+                console.log(_this.phNumber[x]);
+            }
+            console.log(_this.dateOfBirth);
+        };
+        this.getWorkAndEducation = function () {
+            for (var x in _this.education) {
+                console.log(_this.education[x]);
+            }
+            for (var x in _this.occupation) {
+                console.log(_this.occupation[x]);
+            }
+        };
+        this.addNewWork = function (work) {
+            return _this.occupation.push(work);
+        };
+        this.addNewEducation = function (edu) {
+            return _this.education.push(edu);
+        };
+        this.getPlacesLived = function () {
+            for (var x in _this.livesIn) {
+                console.log(_this.livesIn[x]);
+            }
+        };
+        this.addNewPlaces = function (place) {
+            return _this.livesIn.push(place);
+        };
+        this.getContactAndBasicInfo = function () {
+            for (var x in _this.phNumber) {
+                console.log(_this.phNumber[x]);
+            }
+            for (var x in _this.emailAddress) {
+                console.log(_this.emailAddress[x]);
+            }
+            console.log(_this.dateOfBirth);
+            console.log(_this.gender);
+            console.log(_this.interestedIn);
+            for (var x in _this.languages) {
+                console.log(_this.languages[x]);
+            }
+            for (var x in _this.politicalViews) {
+                console.log(_this.politicalViews[x]);
+            }
+            for (var x in _this.religiousViews) {
+                console.log(_this.religiousViews[x]);
+            }
+        };
+        this.addPhnumber = function (phNum) {
+            return _this.phNumber.push(phNum);
+        };
+        this.addEmailAddress = function (email) {
+            return _this.emailAddress.push(email);
+        };
+        this.addLanguages = function (language) {
+            return _this.languages.push(language);
+        };
+        this.addPoliticalView = function (political) {
+            return _this.politicalViews.push(political);
+        };
+        this.addReligiousViews = function (religion) {
+            return _this.religiousViews.push(religion);
+        };
+        this.getFamilyAndRelationshipDetails = function () {
+            console.log(_this.relaionship);
+            for (var x in _this.familyMembers) {
+                console.log(_this.familyMembers[x]);
+            }
+        };
+        this.addFamilyMembers = function (family) {
+            return _this.familyMembers.push(family);
+        };
+        this.getDetailsAboutYou = function () {
+            console.log(_this.aboutYou);
+            for (var x in _this.otherNames) {
+                console.log(_this.otherNames[x]);
+            }
+            console.log(_this.quote);
+            console.log(_this.bloodGroup);
+            console.log(_this.isDonated);
+        };
+        this.addAboutYou = function (about) {
+            _this.aboutYou = about;
+        };
+        this.addOtherNames = function (name) {
+            return _this.otherNames.push(name);
+        };
+        this.getFriendList = function () {
+            for (var x in _this.friendList) {
+                console.log(_this.friendList[x]);
+            }
+        };
+        this.userName = userName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.interestedIn = interestedIn;
+        this.relaionship = this.relaionship;
+        this.profilePic = this.profilePic;
+        this.phNumber = phNumber;
+        this.occupation = occupation;
+        this.livesIn = livesIn;
+        this.languages = languages;
+        this.religiousViews = religiousViews;
+        this.politicalViews = politicalViews;
+        this.familyMembers = familyMembers;
+        this.aboutYou = aboutYou;
+        this.otherNames = otherNames;
+        this.quote = quote;
+        this.bloodGroup = bloodgroup;
+        this.isDonated = isDonated;
+        this.friendList = friendList;
+    }
+    return Social_Profile;
+}());
+var firstVideo = new Youtube_Video(1, "First Youtube video", "www.youtube.com/first_youtube_video", "The greatest channel", 2, "Just a sample Video", "04-09-2018", true);
+var secondVideo = new Youtube_Video(2, "Second Youtube video", "www.youtube.com/second_youtube_video", "The greatest channel", 3, "Just another sample Video", "04-09-2018", true, 200, 15, 2, ["This is awesome", "You suck!!", "Make more", "Best I've seen"]);
+firstVideo.getVideoTitle();
+firstVideo.getNumberOfViews();
+firstVideo.getUpVotes();
+firstVideo.getDownVotes();
+firstVideo.displayComments();
+firstVideo.addComments("Not bad");
+firstVideo.addComments("I've seen better");
+firstVideo.displayComments();
+firstVideo.likeVideo();
+firstVideo.likeVideo();
+firstVideo.hateVideo();
+firstVideo.getUpVotes();
+firstVideo.getDownVotes();
--- main.ts
+++ main.ts
@@ -1,2 +1,296 @@
-console.log("This is type script file")
 
+class Youtube_Video{
+    
+    private videoID : number;
+    private videoTitle : string;
+    private videoURL : string;
+    private channelName : string;
+    private numberOfViews : number;
+    private numberOfUpvotes : number;
+    private numberOfDownVotes : number;
+    private numberOfSubscribers
+    private description : string;
+    private comments : string[];
+    private uploadedOn : string;
+    private isVerified : boolean;
+   
+
+    constructor (videoID : number, videoTitle: string, videoURL: string, channelName: string,numberOfSubscibers: number, description: string, uploadedOn:string, isVerified: boolean, numberOfViews: number = 0, numberOfUpvotes: number = 0, numberOfDownVotes: number = 0, comments: string[] = [""]){
+        this.videoID = videoID;
+        this.videoTitle = videoTitle;
+        this.videoURL = videoURL;
+        this.channelName = channelName;
+        this.numberOfViews = numberOfViews;
+        this.numberOfUpvotes = numberOfUpvotes;
+        this.numberOfDownVotes = numberOfDownVotes;
+        this.numberOfSubscribers = numberOfSubscibers
+        this.description = description;
+        this.comments = comments;
+        this.isVerified = isVerified;
+     
+        
+    }
+    getNumberOfViews = () =>{
+
+        console.log(this.numberOfViews);
+    }
+
+    getRecommendedVideo = () => {
+        console.log("These are your related videos")
+    }
+        
+    getChannelName = () => {
+        console.log(this.channelName);
+    }
+
+    getUpVotes = () => {
+        console.log(this.numberOfUpvotes);
+    }
+
+    getDownVotes = () => {
+        console.log(this.numberOfDownVotes);
+    }
+
+    getNumberOfSubscribers = () => {
+        console.log(this.numberOfSubscribers);
+    }
+
+    getVideoTitle = () => {
+        console.log(this.videoTitle);
+    }
+
+    getDescription = () => {
+        console.log(this.description);
+    }
+
+
+    displayComments = () =>{
+        
+        
+        let numberOfComments = this.comments.length;
+        
+        console.log(numberOfComments);
+        for (let x in this.comments){
+            console.log(this.comments[x])
+        }
+    }
+
+    addComments = (comment : string) =>{
+        this.comments.push(comment);
+    }
+
+    likeVideo = () =>{
+        this.numberOfUpvotes+=1;
+    }
+
+    hateVideo = () => {
+        this.numberOfDownVotes+=1;
+    }
+
+    subscribeChannel = () => {
+        this.numberOfSubscribers+=1;
+    }
+
+    shareVideo = () =>{
+        console.log(this.videoURL);
+    } 
+
+}
+
+
+
+class Social_Profile {
+
+    private userName : string
+    private profilePic : string;
+    private phNumber : string[];
+    private dateOfBirth : string;
+    private occupation : string[];
+    private education : string[];
+    private livesIn : string[];
+    private emailAddress : string[];
+    private gender : string;
+    private interestedIn : string;
+    private languages : string[];
+    private religiousViews : string[];
+    private politicalViews : string[];
+    private relaionship : string;
+    private familyMembers : string[];
+    private aboutYou : string;
+    private otherNames : string[];
+    private quote : string;
+    private bloodGroup : string;
+    private isDonated : boolean;
+    private friendList : string[];
+    
+    constructor (userName : string, dateOfBirth : string, gender : string, interestedIn : string, relationship : string, prifilePic ?: string, phNumber ?: string[], occupation ?: string[], livesIn ?: string[], languages ?: string[], religiousViews ?: string[], politicalViews ?: string[], familyMembers ?: string[], aboutYou ?: string, otherNames ?: string[], quote ?: string, bloodgroup ?: string, isDonated ?: boolean, friendList ?: string[]){
+        
+        this.userName = userName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.interestedIn = interestedIn;
+        this.relaionship = this.relaionship;
+        this.profilePic = this.profilePic;
+        this.phNumber = phNumber;
+        this.occupation = occupation;
+        this.livesIn = livesIn;
+        this.languages = languages;
+        this.religiousViews = religiousViews;
+        this.politicalViews = politicalViews;
+        this.familyMembers = familyMembers;
+        this.aboutYou = aboutYou;
+        this.otherNames = otherNames;
+        this.quote = quote;
+        this.bloodGroup = bloodgroup;
+        this.isDonated = isDonated;
+        this.friendList = friendList;
+    }
+
+    getOverView = () => {
+        let lastOc = this.occupation.length;
+        console.log(this.occupation[lastOc-1]);
+        
+        let lastEd = this.education.length;
+        console.log(this.education[lastEd-1]);
+
+        let lastCy = this.livesIn.length;
+        console.log(this.livesIn[lastCy-1]);
+
+        for (let x in this.phNumber){
+            console.log(this.phNumber[x]);
+        }
+
+        console.log(this.dateOfBirth);
+
+    }
+
+
+
+    getWorkAndEducation = () => {
+        for (let x in this.education ){
+            console.log(this.education[x])
+        }
+
+        for (let x in this.occupation){
+            console.log(this.occupation[x])
+        }
+    }
+    
+    addNewWork = (work : string) => {
+        return this.occupation.push(work);
+    }
+
+    addNewEducation = (edu : string) => {
+        return this.education.push(edu);
+    }
+
+    getPlacesLived = () => {    
+        for (let x in this.livesIn){
+            console.log(this.livesIn[x])
+        }
+    }
+
+    addNewPlaces = (place : string) => {
+        return this.livesIn.push(place);
+    }
+
+    getContactAndBasicInfo = () => {
+        for (let x in this.phNumber){
+            console.log(this.phNumber[x]);
+        }
+
+        for (let x in this.emailAddress){
+            console.log(this.emailAddress[x])
+        }
+
+        console.log (this.dateOfBirth);
+        console.log (this.gender);
+        console.log(this.interestedIn);
+        for (let x in this.languages){
+            console.log(this.languages[x])
+        }
+        for (let x in this.politicalViews){
+            console.log(this.politicalViews[x])
+        }
+        for (let x in this.religiousViews){
+            console.log(this.religiousViews[x])
+        }
+    }
+
+    addPhnumber = (phNum : string) =>{
+        return this.phNumber.push(phNum);
+    }
+
+    addEmailAddress = (email : string) => {
+        return this.emailAddress.push(email);
+    }
+
+    addLanguages = (language : string) => {
+        return this.languages.push(language);
+    }
+
+    addPoliticalView = (political : string) => {
+        return this.politicalViews.push(political);
+    }
+
+    addReligiousViews = (religion : string) => {
+        return this.religiousViews.push(religion);
+    }
+    
+    getFamilyAndRelationshipDetails = () => {
+        console.log(this.relaionship);
+
+        for(let x in this.familyMembers){
+            console.log(this.familyMembers[x]);
+        }
+    }
+
+    addFamilyMembers = (family : string) => {
+        return this.familyMembers.push(family);
+    }
+
+    getDetailsAboutYou = () => {
+        console.log(this.aboutYou);
+
+        for (let x in this.otherNames){
+            console.log(this.otherNames[x]);
+        }
+
+        console.log(this.quote);
+
+        console.log(this.bloodGroup);
+
+        console.log(this.isDonated);
+    }
+
+    addAboutYou = (about : string) => {
+        this.aboutYou = about;
+    }
+
+    addOtherNames = (name : string) => {
+        return this.otherNames.push(name);
+    }
+
+    getFriendList = () => {
+        for (let x in this.friendList){
+            console.log(this.friendList[x])
+        }
+    }
+}
+    
+let firstVideo = new Youtube_Video(1,"First Youtube video", "www.youtube.com/first_youtube_video", "The greatest channel", 2, "Just a sample Video", "04-09-2018", true)
+let secondVideo = new Youtube_Video(2,"Second Youtube video", "www.youtube.com/second_youtube_video", "The greatest channel", 3, "Just another sample Video", "04-09-2018", true, 200, 15, 2, ["This is awesome", "You suck!!", "Make more", "Best I've seen"])
+
+firstVideo.getVideoTitle();
+firstVideo.getNumberOfViews();
+firstVideo.getUpVotes();
+firstVideo.getDownVotes();
+firstVideo.displayComments();
+firstVideo.addComments("Not bad");
+firstVideo.addComments("I've seen better");
+firstVideo.displayComments();
+firstVideo.likeVideo();
+firstVideo.likeVideo();
+firstVideo.hateVideo();
+firstVideo.getUpVotes();
+firstVideo.getDownVotes();
